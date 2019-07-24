40c6cfadb90eed40691c7b2d2fe8f1a92cc25ab5
Tirado <tirada@rpi.edu>
2019-07-24 11:20:22 -0700

    Added Theming and Content

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { OptionsPanel } from './components/OptionsPanel';
+import { Dialog, DialogTitle, DialogContent, createMuiTheme, Grid } from '@material-ui/core';
+import { ThemeProvider } from '@material-ui/styles';
+import { SelectedContent } from './components/SelectedContent';
 
 const App: React.FC = () => {
+  const theme = createMuiTheme();
   return (
-    <div>
-      <OptionsPanel />
-    </div>
+    <ThemeProvider theme={theme}>
+    <Dialog open={true} scroll='paper' >
+      <DialogTitle>Create Video</DialogTitle>
+      <DialogContent dividers>
+        <Grid container>
+          <Grid item xs={8}>
+            <OptionsPanel />
+          </Grid>
+          <Grid item xs={4}>
+            <SelectedContent />
+          </Grid>
+        </Grid>
+      </DialogContent>
+    </Dialog>
+    </ThemeProvider>
   );
 }
 
